56a3c0258ed4e0403521fc934008b3cee62a2aad
mamhmoud Abdelwahab <mahmoud.elziany@gmail.com>
2020-06-29 02:47:32 +0200

    بسم الله الرحمن الرحيم
    Co-Authored-By: Mahmoud-Abdelwahab <mahmoud-abdelwahab@users.noreply.github.com>

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by kasper on 6/29/20.
+//  Copyright © 2020 Mahmoud.Abdul-Wahab.Messenger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
